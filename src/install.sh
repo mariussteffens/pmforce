65648eda9c04f240aff2ec161f02cf133cefe70c
Marius Steffens <marius.steffens@cispa.saarland>
2021-05-12 12:55:41 +0200

    add missing pre-bundled Analysis pipeline, pin correct version of webpack in the installer script

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 rm -rf node_modules
 rm package-lock.json
-npm install argparse psl pg readline-sync event-stream readline-sync md5 bufferutil webpack webpack-cli puppeteer
+npm install argparse psl pg readline-sync event-stream readline-sync md5 bufferutil puppeteer
+npm install -g --save-dev webpack-cli@3.3.12 webpack@4.46
 mkdir log
 mkdir err
